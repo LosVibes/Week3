9f581cdf285ea467ff58af16ec81fb49f7a7d831
Juan Carlos Hernandez <hernandezjuanjc@gmail.com>
2023-10-18 13:52:11 -0500

    update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-// Use ` Backtick below tilde to create a jas text temp
-const menu = ``;
-<ul>
-<li><a href="index.html">Home</a></li>
-<li><a href="index.html">About</a></li>
-</ul>
+
+username = "Kevin"
+
+console.log(username)
+console.log(username)
+console.log(username)
