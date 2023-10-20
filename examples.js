4a89853e0ba5cabeb24c788be107980ee23e07ac
Juan Carlos Hernandez <hernandezjuanjc@gmail.com>
2023-10-20 14:56:52 -0500

    cal

--- examples.js
+++ examples.js
@@ -16,3 +16,13 @@ default:
 dayName = "<unknown>";
 }
 console.log( dayName )
+
+let orderNum = 10254;
+let orderStatus = "processing";
+let message =
+
+"Order " + orderNum + "'s status is: " + orderStatus;
+const orderStatusPara =
+
+document.getElementById("orderStatusPara");
+orderStatusPara.innerHTML = message;
--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 
-username = "Kevin"
+function onCalculate(){
+    const amountValue = Number(amount.value);
+    const rateValue = Number(rate.value);
+    const yearsValue = Number(years.value);
 
-console.log(username)
-console.log(username)
-console.log(username)
+    const apr = rateValue / 1200 // months and integer percent to fraction 
+    const term = yearsValue *12 // months 
+    const amt = amountValue;
+
+    const payment = amt*(apr * Math.pow((1 + apr), term))/(Math.pow((1 + apr), term) - 1);
+    output.innerText = payment;
+}
